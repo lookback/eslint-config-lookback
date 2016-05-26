aa7fe3a0e6a0affd0179a613c86992a3b7cebac1
Johan Brook <johan.jvb@gmail.com>
2016-05-26 14:54:43 +0200

    Remove new-cap rule override for Meteor

--- rules/meteor.js
+++ rules/meteor.js
@@ -12,13 +12,6 @@ module.exports = {
      */
     // babel inserts "use strict"; for us
     // http://eslint.org/docs/rules/strict
-    strict: [2, "never"],
-
-    // allows certain non-constructor functions to start with a capital letter
-    "new-cap": [2, {                // http://eslint.org/docs/rules/new-cap
-      capIsNewExceptions: [
-        "Match", "Any", "Object", "ObjectIncluding", "OneOf", "Optional", "Where"
-      ]
-    }]
+    strict: [2, "never"]
   }
 };
